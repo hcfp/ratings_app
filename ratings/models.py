f10f5f4859f641f607a21b7f553a28b90ec66efe
Habib Chanchwelia <sc19hc@leeds.ac.uk>
2022-02-27 16:35:02 +0000

    Initial commit

---
+++ ratings/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Module(models.Model):
+    pass
+
+class ModuleLeaders(models.Model):
+    pass
+
+class Professors(models.Model):
+    pass
+
